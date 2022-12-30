07c23e9763612bece0165e3ae22065940f5a5140
CordHouse <qkqktl5310@gmail.com>
2022-12-30 17:50:05 +0900

    feat : (Failure) - void init

---
+++ src/main/java/com/example/hospitalproject/Response/Failure.java
@@ -0,0 +1,12 @@
+package com.example.hospitalproject.Response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@Getter
+public class Failure implements Result{
+    private String msg;
+}
