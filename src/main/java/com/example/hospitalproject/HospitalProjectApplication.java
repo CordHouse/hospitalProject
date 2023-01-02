989fffb615c015e6fa32866f407dca83e4568e1a
CordHouse <qkqktl5310@gmail.com>
2023-01-02 20:36:40 +0900

    refactor : (HospitalProjectApplication) - 시큐리티 일시 제한

--- src/main/java/com/example/hospitalproject/HospitalProjectApplication.java
+++ src/main/java/com/example/hospitalproject/HospitalProjectApplication.java
@@ -2,8 +2,9 @@ package com.example.hospitalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HospitalProjectApplication {
 
     public static void main(String[] args) {
