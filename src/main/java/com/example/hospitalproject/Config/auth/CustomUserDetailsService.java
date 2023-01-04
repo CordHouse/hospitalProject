23c2848d7459f0a311a7ae5276e18bdf115b8c7f
chrkb1569 <chrkb1569@naver.com>
2023-01-04 18:22:41 +0900

    feat : Add Security Files

--- src/main/java/com/example/hospitalproject/Config/auth/CustomUserDetailsService.java
+++ src/main/java/com/example/hospitalproject/Config/auth/CustomUserDetailsService.java
@@ -1,4 +1,35 @@
 package com.example.hospitalproject.Config.auth;
 
-public class CustomUserDetailsService {
+import com.example.hospitalproject.Entity.User.User;
+import com.example.hospitalproject.Repository.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "을 찾지 못하였습니다."));
+    }
+
+    public UserDetails createUserDetails(User user) {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getUserGrade().toString());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singleton(simpleGrantedAuthority));
+    }
 }
--- src/main/java/com/example/hospitalproject/Config/security/SecurityConfigure.java
+++ src/main/java/com/example/hospitalproject/Config/security/SecurityConfigure.java
@@ -1,4 +1,50 @@
 package com.example.hospitalproject.Config.security;
 
+import com.example.hospitalproject.Config.jwt.JwtAccessDeniedHandler;
+import com.example.hospitalproject.Config.jwt.JwtAuthenticationEntryPointHandler;
+import com.example.hospitalproject.Config.jwt.JwtSecurityConfig;
+import com.example.hospitalproject.Config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfigure {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPointHandler jwtAuthenticationEntryPointHandler;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPointHandler)
+
+                .and()
+                .logout().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/home/signIn", "/home/signup").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return httpSecurity.build();
+    }
 }
