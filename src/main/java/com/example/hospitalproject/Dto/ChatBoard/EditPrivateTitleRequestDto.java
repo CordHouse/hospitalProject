b48b90f4ef61401b446b7b2fd5b34b6c65c338e3
CordHouse <qkqktl5310@gmail.com>
2023-01-05 01:12:32 +0900

    feat : (EditPrivateTitleRequestDto) - 채팅방 제목 변경

---
+++ src/main/java/com/example/hospitalproject/Dto/ChatBoard/EditPrivateTitleRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.hospitalproject.Dto.ChatBoard;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EditPrivateTitleRequestDto {
+    @NotBlank(message = "변경할 제목을 입력해주세요.")
+    private String changeTitle;
+}
