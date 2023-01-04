29ce1f193b46ccb6246a1d943250c8747ccd695c
chrkb1569 <chrkb1569@naver.com>
2023-01-04 18:21:19 +0900

    feat : Add RefreshToken Entity & Repository

---
+++ src/main/java/com/example/hospitalproject/Repository/RefreshToken/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.hospitalproject.Repository.RefreshToken;
+
+import com.example.hospitalproject.Entity.User.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+}
