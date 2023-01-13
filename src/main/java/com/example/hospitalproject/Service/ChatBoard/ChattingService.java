135482e754e64b5a9832b89645c22bdfc2a7fbc7
CordHouse <qkqktl5310@gmail.com>
2023-01-13 22:00:24 +0900

    feat : (ChattingService) - 채팅방 목록 추가 - 채팅 삭제 추가

--- src/main/java/com/example/hospitalproject/Service/ChatBoard/ChattingService.java
+++ src/main/java/com/example/hospitalproject/Service/ChatBoard/ChattingService.java
@@ -1,14 +1,20 @@
 package com.example.hospitalproject.Service.ChatBoard;
 
 import com.example.hospitalproject.Dto.ChatBoard.ChattingCommentRequestDto;
+import com.example.hospitalproject.Dto.ChatBoard.ChattingListResponseDto;
 import com.example.hospitalproject.Entity.Chatting.ChatBoard;
 import com.example.hospitalproject.Entity.Chatting.Chatting;
 import com.example.hospitalproject.Exception.ChatBoard.NotFoundChatBoardException;
 import com.example.hospitalproject.Repository.ChatBoard.ChatBoardRepository;
 import com.example.hospitalproject.Repository.ChatBoard.ChattingRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +29,24 @@ public class ChattingService {
         });
         Chatting chatting = new Chatting(chattingCommentRequestDto.getComment(), chatBoard);
         chattingRepository.save(chatting);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ChattingListResponseDto> getMyChattingList(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<ChatBoard> chatBoards = chatBoardRepository.findAllBySenderOrReceiver(authentication.getName(), authentication.getName()).orElseThrow(() -> {
+            throw new NotFoundChatBoardException();
+        });
+        List<ChattingListResponseDto> myChattingList = new LinkedList<>();
+        chatBoards.forEach(board -> myChattingList.add(new ChattingListResponseDto().toDo(board)));
+        return myChattingList;
+    }
+
+    @Transactional
+    public void chatDelete(long id){
+        Chatting chatting = chattingRepository.findById(id).orElseThrow(() -> {
+            throw new NotFoundChatBoardException();
+        });
+        chatting.setDoDelete("true");
     }
 }
