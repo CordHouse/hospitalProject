76fd16d87fffe282bdf2ee05dde5831a916c5e69
CordHouse <qkqktl5310@gmail.com>
2023-01-13 21:55:20 +0900

    feat : (ChatBoardRepository) - sender와 receiver로 유저 찾기 추가

--- src/main/java/com/example/hospitalproject/Repository/ChatBoard/ChatBoardRepository.java
+++ src/main/java/com/example/hospitalproject/Repository/ChatBoard/ChatBoardRepository.java
@@ -4,6 +4,10 @@ import com.example.hospitalproject.Entity.Chatting.ChatBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ChatBoardRepository extends JpaRepository<ChatBoard, Long> {
+    Optional<List<ChatBoard>> findAllBySenderOrReceiver(String sender, String receiver);
 }
