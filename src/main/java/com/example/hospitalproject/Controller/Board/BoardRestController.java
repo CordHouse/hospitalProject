72c01f5d47fdacd44143ecb7ff929af9d20d3bda
JuneBori <jb990628@gmail.com>
2023-01-17 21:54:20 +0900

    feat : (BoardRestController) - 게시판 생성

--- src/main/java/com/example/hospitalproject/Controller/Board/BoardRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/Board/BoardRestController.java
@@ -1,4 +1,23 @@
 package com.example.hospitalproject.Controller.Board;
 
+import com.example.hospitalproject.Dto.Board.BoardCreateRequestDto;
+import com.example.hospitalproject.Response.Response;
+import com.example.hospitalproject.Service.Board.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class BoardRestController {
+    private final BoardService boardService;
+
+    @PostMapping("/board")
+    @ResponseStatus(HttpStatus.OK)
+    public Response create(@RequestBody @Valid BoardCreateRequestDto boardCreateRequestDto){
+        return Response.success();
+    }
 }
