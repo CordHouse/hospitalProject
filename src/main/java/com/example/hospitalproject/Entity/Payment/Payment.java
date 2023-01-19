8ae9eb49f1edd015f67d8f6a70449886ccc33c40
CordHouse <qkqktl5310@gmail.com>
2023-01-19 21:59:20 +0900

    refactor : (Payment) - Payment entity init

--- src/main/java/com/example/hospitalproject/Entity/Payment/Payment.java
+++ src/main/java/com/example/hospitalproject/Entity/Payment/Payment.java
@@ -1,4 +1,29 @@
 package com.example.hospitalproject.Entity.Payment;
 
+import com.example.hospitalproject.Entity.Payment.Credit.Card;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+//    private GroupCode groupCode;
+//
+//    private Code code;
+//
+//    private Card card;
+//
+//    private String approval;
 }
