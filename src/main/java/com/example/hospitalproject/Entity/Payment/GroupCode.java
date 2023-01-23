153bb83f5bf7a69e602edf9fba54ae7930e04165
CordHouse <qkqktl5310@gmail.com>
2023-01-24 03:21:09 +0900

    feat : (GroupCode) - 그룹 코드 추가 (카드, 상품권, 현금, 오류)

--- src/main/java/com/example/hospitalproject/Entity/Payment/GroupCode.java
+++ src/main/java/com/example/hospitalproject/Entity/Payment/GroupCode.java
@@ -1,4 +1,27 @@
 package com.example.hospitalproject.Entity.Payment;
 
-public class GroupCode {
+import java.util.Arrays;
+
+public enum GroupCode {
+    CARD(100),
+    GIFT_CARD(200),
+    CASH(300),
+    ERROR(0);
+
+    private int groupCode;
+
+    GroupCode(int groupCode){
+        this.groupCode = groupCode;
+    }
+
+    public static GroupCode findGroupCode(int code){
+        return Arrays.stream(GroupCode.values())
+                .filter(group -> group.getGroupCode() == code)
+                .findAny()
+                .orElse(ERROR);
+    }
+
+    public int getGroupCode(){
+        return groupCode;
+    }
 }
