2d53051a011812eb79a44f5944dca40686101de3
CordHouse <qkqktl5310@gmail.com>
2023-01-24 03:18:29 +0900

    feat : (CardRepository) - findByUser

--- src/main/java/com/example/hospitalproject/Repository/Payment/Card/CardRepository.java
+++ src/main/java/com/example/hospitalproject/Repository/Payment/Card/CardRepository.java
@@ -1,6 +1,7 @@
 package com.example.hospitalproject.Repository.Payment.Card;
 
 import com.example.hospitalproject.Entity.Payment.Credit.Card;
+import com.example.hospitalproject.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
     Optional<List<Card>> findByCardNumber(String cardNumber);
+    Card findByUser(User user);
 }
