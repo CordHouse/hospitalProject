30e1a4a9845c05cc8120a67d12cc8a1581ff1b88
CordHouse <qkqktl5310@gmail.com>
2023-01-24 03:13:46 +0900

    refactor : (UserRegisterRequestDto) - 비밀번호 message 수정

--- src/main/java/com/example/hospitalproject/Dto/User/UserRegisterRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/User/UserRegisterRequestDto.java
@@ -19,7 +19,7 @@ public class UserRegisterRequestDto {
     private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$", message = "8이상 20자리 특수문자, 영문, 숫자 조합")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$", message = "비밀번호는 8이상 20자리 특수문자, 영문, 숫자 조합 형식을 지켜주세요.")
     private String password;
 
     @NotBlank(message = "생일을 입력해주세요.")
