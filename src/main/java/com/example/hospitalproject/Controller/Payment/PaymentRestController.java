777df1332b3eb4946e2769ff62e6a0af77d2b1f6
CordHouse <qkqktl5310@gmail.com>
2023-01-24 03:23:31 +0900

    feat : (PaymentController) - 결제 진행

--- src/main/java/com/example/hospitalproject/Controller/Payment/PaymentRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/Payment/PaymentRestController.java
@@ -1,13 +1,26 @@
 package com.example.hospitalproject.Controller.Payment;
 
+import com.example.hospitalproject.Dto.Payment.Card.PayChargeRequestDto;
 import com.example.hospitalproject.Service.Payment.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/payment")
 public class PaymentRestController {
     private final PaymentService paymentService;
+
+    /**
+     * 결제 진행
+     * @param payChargeRequestDto
+     */
+    @PostMapping("/charge")
+    @ResponseStatus(HttpStatus.OK)
+    public void payCharge(@RequestBody @Valid PayChargeRequestDto payChargeRequestDto){
+        paymentService.payCharge(payChargeRequestDto);
+    }
 }
