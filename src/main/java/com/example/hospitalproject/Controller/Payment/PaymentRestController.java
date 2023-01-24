e1dc86a37ccd216440055e92a3771c6473af2786
CordHouse <qkqktl5310@gmail.com>
2023-01-24 21:00:13 +0900

    feat : (PaymentRestController) - 결제 취소 - 결제 승인 (관리자, 매니저)

--- src/main/java/com/example/hospitalproject/Controller/Payment/PaymentRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/Payment/PaymentRestController.java
@@ -18,9 +18,29 @@ public class PaymentRestController {
      * 결제 진행
      * @param payChargeRequestDto
      */
-    @PostMapping("/charge")
+    @PostMapping("/common/charge")
     @ResponseStatus(HttpStatus.OK)
     public void payCharge(@RequestBody @Valid PayChargeRequestDto payChargeRequestDto){
         paymentService.payCharge(payChargeRequestDto);
+    }
+
+    /**
+     * 결제 취소
+     * @param id
+     */
+    @PostMapping("/common/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void payCancel(@PathVariable long id){
+        paymentService.payCancel(id);
+    }
+
+    /**
+     * 결제 취소 승인
+     * @param id
+     */
+    @PostMapping("/manager/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void payToDoApproval(@PathVariable long id){
+        paymentService.payToDoApproval(id);
     }
 }
