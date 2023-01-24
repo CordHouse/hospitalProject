3776ef6a0abb6a85e01b7f3f9d9585f7e0d6e8e0
CordHouse <qkqktl5310@gmail.com>
2023-01-24 20:57:56 +0900

    feat : (PayCancelException)

---
+++ src/main/java/com/example/hospitalproject/Exception/Payment/PayCancelException.java
@@ -0,0 +1,7 @@
+package com.example.hospitalproject.Exception.Payment;
+
+public class PayCancelException extends RuntimeException{
+    public PayCancelException(String message){
+        super(message);
+    }
+}
