35b4c5175d5a18f1b09bc4a3591d5d7ca8b275e1
Jin-yujin <yjinj0383@naver.com>
2023-01-25 21:52:21 +0900

    feat : (CommentService) - 댓글 생성

--- src/main/java/com/example/hospitalproject/Service/Comment/CommentService.java
+++ src/main/java/com/example/hospitalproject/Service/Comment/CommentService.java
@@ -1,4 +1,26 @@
 package com.example.hospitalproject.Service.Comment;
 
+import com.example.hospitalproject.Dto.Comment.CommentCreateRequestDto;
+import com.example.hospitalproject.Entity.Comment.Comment;
+import com.example.hospitalproject.Repository.Comment.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Transactional  //데이터베이스에 저장하기 전 안전장치
+    public void commentCreate(CommentCreateRequestDto commentCreateRequestDto){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); //로그인한 아이디 쓰고싶을때
+        Comment comment = new Comment(commentCreateRequestDto.getComment(), authentication.getName());
+        commentRepository.save(comment);
+
+    }
+
 }
