0ff273308970ea9ba34800042eddbd9d0565d3c7
JuneBori <jb990628@gmail.com>
2023-01-27 23:35:49 +0900

    feat : (BoardChangeRequestDto) - 게시글 변경 Dto

---
+++ src/main/java/com/example/hospitalproject/Dto/Board/BoardChangeRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.hospitalproject.Dto.Board;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardChangeRequestDto {
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해주세요.")
+    private String content;
+}
