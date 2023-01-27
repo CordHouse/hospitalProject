cf545f03131df6c3b242c8f0fcea408b047801f0
CordHouse <qkqktl5310@gmail.com>
2023-01-27 16:25:06 +0900

    feat : (CardService) - 등록된 카드 리스트 조회 - 현재 선택한 카드 조회 - 선택한 카드 변경

--- src/main/java/com/example/hospitalproject/Service/Payment/Card/CardService.java
+++ src/main/java/com/example/hospitalproject/Service/Payment/Card/CardService.java
@@ -1,13 +1,12 @@
 package com.example.hospitalproject.Service.Payment.Card;
 
+import com.example.hospitalproject.Dto.Payment.Card.CardChangeResponseDto;
 import com.example.hospitalproject.Dto.Payment.Card.CardInfoRequestDto;
+import com.example.hospitalproject.Dto.Payment.Card.CardInquiryResponseDto;
 import com.example.hospitalproject.Entity.Payment.Credit.BankType;
 import com.example.hospitalproject.Entity.Payment.Credit.Card;
 import com.example.hospitalproject.Entity.User.User;
-import com.example.hospitalproject.Exception.Payment.DuplicateCardInfoException;
-import com.example.hospitalproject.Exception.Payment.NotFoundBankException;
-import com.example.hospitalproject.Exception.Payment.NotFoundCardException;
-import com.example.hospitalproject.Exception.Payment.NotFoundCardListException;
+import com.example.hospitalproject.Exception.Payment.*;
 import com.example.hospitalproject.Exception.UserException.NotFoundUsernameException;
 import com.example.hospitalproject.Repository.Payment.Card.CardRepository;
 import com.example.hospitalproject.Repository.User.UserRepository;
@@ -17,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -27,7 +27,6 @@ public class CardService {
 
     /**
      * 카드 등록하기
-     * @param cardInfoRequestDto
      */
     @Transactional
     public void cardRegistration(CardInfoRequestDto cardInfoRequestDto){
@@ -50,7 +49,6 @@ public class CardService {
 
     /**
      * 등록된 카드 중 선택 할 수 있도록 기능 추가
-     * @param id
      */
     @Transactional
     public void cardChoice(long id){
@@ -63,8 +61,59 @@ public class CardService {
     }
 
     /**
+     * 등록한 카드 리스트 조회
+     */
+    @Transactional(readOnly = true)
+    public List<CardInquiryResponseDto> getMyCardList(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Card> card = cardRepository.findAllByUser(userRepository.findByUsername(authentication.getName()).orElseThrow(() -> {
+            throw new NotFoundUsernameException("해당 유저가 존재하지 않습니다.");
+        }));
+        List<CardInquiryResponseDto> cardList = new LinkedList<>();
+        card.forEach(value -> cardList.add(new CardInquiryResponseDto().toDo(value)));
+        return cardList;
+    }
+
+    /**
+     * 사용할 카드로 선택한 카드 조회
+     */
+    @Transactional(readOnly = true)
+    public CardInquiryResponseDto getMyCard(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Card> card = cardRepository.findAllByUser(userRepository.findByUsername(authentication.getName()).orElseThrow(() -> {
+            throw new NotFoundUsernameException("해당 유저가 존재하지 않습니다.");
+        }));
+        if(card.stream().filter(value -> value.getSelectCard().equals("선택")).count() == 1){
+            return new CardInquiryResponseDto().toDo(card.stream().filter(value -> value.getSelectCard().equals("선택")).findAny().orElseThrow());
+        }
+        throw new NotFoundCardChoiceException();
+    }
+
+    /**
+     * 사용할 카드 변경
+     */
+    @Transactional
+    public CardChangeResponseDto changeMyChoiceCard(long id){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 이전에 선택된 카드
+        Card cardBefore = cardRepository.findById(getMyCard().getId()).orElseThrow(() -> {
+            throw new NotFoundCardException("카드가 존재하지 않습니다.");
+        });
+        cardBefore.setSelectCard("미선택");
+        usernameAuthCompareCheck(authentication.getName(), cardBefore.getUser().getUsername());
+        CardInquiryResponseDto beforeChoiceCard = new CardInquiryResponseDto().toDo(cardBefore);
+        // 변경 후 선택된 카드
+        Card cardAfter = cardRepository.findById(id).orElseThrow(() -> {
+            throw new NotFoundCardException("등록된 카드가 존재하지 않습니다.");
+        });
+        cardAfter.setSelectCard("선택");
+        usernameAuthCompareCheck(authentication.getName(), cardAfter.getUser().getUsername());
+        CardInquiryResponseDto afterChoiceCard = new CardInquiryResponseDto().toDo(cardAfter);
+        return new CardChangeResponseDto().toDo(beforeChoiceCard, afterChoiceCard);
+    }
+
+    /**
      * enum 타입으로 등록될 수 있는 은행인지 확인
-     * @param bank
      */
     protected void bankTypeCheck(String bank){
         if(BankType.searchBankType(bank).getBankName().equals("없음")){
@@ -74,7 +123,6 @@ public class CardService {
 
     /**
      * 입력과 똑같은 카드 리스트 추출
-     * @param cardInfoRequestDto
      */
     protected void cardInfoMatchCheck(CardInfoRequestDto cardInfoRequestDto){
         List<Card> cardList = cardRepository.findByCardNumber(cardInfoRequestDto.getCardNumber()).orElseThrow();
@@ -83,8 +131,6 @@ public class CardService {
 
     /**
      * 이미 등록된 카드인지 확인
-     * @param card
-     * @param cardInfoRequestDto
      */
     protected void cardNumberCheck(Card card, CardInfoRequestDto cardInfoRequestDto){
         if(cardInfoRequestDto.getBank().equals(card.getBank())
@@ -96,8 +142,6 @@ public class CardService {
 
     /**
      * 카드 등록시 바로 사용할 카드로 선택할 것인지 선택여부
-     * @param cardChoice
-     * @return
      */
     protected String selectCardCheck(String cardChoice){
         if(!cardChoice.equals("선택")){
@@ -108,8 +152,6 @@ public class CardService {
 
     /**
      * 본인 카드 목록 중에 선택된 카드가 있는지 확인하는 함수 (등록될 카드는 한장이여야 한다.)
-     * @param id
-     * @return
      */
     protected boolean selectCardListCheck(long id){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -125,4 +167,15 @@ public class CardService {
         }
         return false;
     }
+
+    /**
+     * 카드 변경할때 본인이 등록한 카드인지 확인하고 바꾸기
+     */
+    protected void usernameAuthCompareCheck(String username, String compareUsername){
+        if(username.equals(compareUsername)){
+            return;
+        }
+        throw new NotFoundUsernameException("카드 등록자와 일치하지 않습니다.");
+    }
+
 }
