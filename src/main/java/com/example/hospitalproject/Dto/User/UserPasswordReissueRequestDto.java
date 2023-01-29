b228c8473ee74be1117247e8c63cccfad69c2748
chrkb1569 <chrkb1569@naver.com>
2023-01-29 15:09:35 +0900

    feat : Add Annotation to DTO Files

--- src/main/java/com/example/hospitalproject/Dto/User/UserPasswordReissueRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/User/UserPasswordReissueRequestDto.java
@@ -2,11 +2,13 @@ package com.example.hospitalproject.Dto.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserPasswordReissueRequestDto {
     @NotBlank(message = "사용자의 비밀번호를 재발급 받기 위한 사용자 아이디를 입력해주세요")
     private String username;
