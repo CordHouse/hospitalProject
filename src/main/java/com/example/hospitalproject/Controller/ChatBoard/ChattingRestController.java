2240fbe289abb5dac72b185876293945b75d24b4
CordHouse <qkqktl5310@gmail.com>
2023-01-31 01:17:20 +0900

    docs : (ChattingRestController) - 주석추가

--- src/main/java/com/example/hospitalproject/Controller/ChatBoard/ChattingRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/ChatBoard/ChattingRestController.java
@@ -15,18 +15,27 @@ import javax.validation.Valid;
 public class ChattingRestController {
     private final ChattingService chattingService;
 
+    /**
+     * 채팅방(id)에서 채팅하기
+     */
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     private void chatRunStatus(@PathVariable Long id, @RequestBody @Valid ChattingCommentRequestDto chattingCommentRequestDto){
         chattingService.chatRunStatus(id, chattingCommentRequestDto);
     }
 
+    /**
+     * 채팅방 안의 채팅 조회
+     */
     @GetMapping("/{id}/comment")
     @ResponseStatus(HttpStatus.OK)
     private Response getMyChattingList(@PathVariable long id){
         return Response.success(chattingService.getMyChattingList(id));
     }
 
+    /**
+     * 채팅 삭제
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     private void chatDelete(@PathVariable Long id){
