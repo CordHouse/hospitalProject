f2cdf38ad700f575ae2c0f5902b1b2cdc27a0787
chrkb1569 <chrkb1569@naver.com>
2023-01-30 16:31:56 +0900

    feat : Add Pattern Restriction to DTO Columns

--- src/main/java/com/example/hospitalproject/Dto/User/UserPasswordReissueRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/User/UserPasswordReissueRequestDto.java
@@ -5,17 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserPasswordReissueRequestDto {
     @NotBlank(message = "사용자의 비밀번호를 재발급 받기 위한 사용자 아이디를 입력해주세요")
+    @Pattern(regexp = "^[0-9a-zA-Z]{2,10}", message = "입력하신 아이디를 다시 한 번 확인해주세요")
     private String username;
 
     @NotBlank(message = "사용자 비밀번호를 재발급 받기 위한 사용자 이메일을 입력해주세요")
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "입력하신 이메일을 다시 한번 확인해주세요")
     private String email;
 
     @NotBlank(message = "사용자 비밀번호를 재발급 받기 위한 사용자 전화 번호를 입력해주세요")
+    @Pattern(regexp = "\\d{2,3}-\\d{3,4}-\\d{4}", message = "입력하신 전화번호를 다시 한 번 확인해주세요")
     private String phone;
 }
