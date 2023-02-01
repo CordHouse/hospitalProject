04c8b41041001a53defe9e9ead91165ca1044e19
JuneBori <jb990628@gmail.com>
2023-02-01 22:00:05 +0900

    feat : (BoardRestController) - 게시물 조회 작성

--- src/main/java/com/example/hospitalproject/Controller/Board/BoardRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/Board/BoardRestController.java
@@ -2,6 +2,7 @@ package com.example.hospitalproject.Controller.Board;
 
 import com.example.hospitalproject.Dto.Board.BoardChangeRequestDto;
 import com.example.hospitalproject.Dto.Board.BoardCreateRequestDto;
+import com.example.hospitalproject.Response.Response;
 import com.example.hospitalproject.Service.Board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,23 +16,46 @@ import javax.validation.Valid;
 public class BoardRestController {
     private final BoardService boardService;
 
+    /**
+     * 게시물 생성
+     * 댓글 생성 검증
+     * @param boardCreateRequestDto
+     */
     @PostMapping("/board")
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody @Valid BoardCreateRequestDto boardCreateRequestDto){
         boardService.create(boardCreateRequestDto);
     }
 
+    /**
+     * 게시물 삭제
+     * 댓글 삭제 검증
+     * @param id
+     */
     @DeleteMapping("/board/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Long id){
         boardService.delete(id);
     }
 
+    /**
+     * 게시물 수정
+     * 댓글 수정 검증
+     * @param id
+     * @param boardChangeRequestDto
+     */
     @PutMapping("/board/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void change(@PathVariable Long id, @RequestBody @Valid BoardChangeRequestDto boardChangeRequestDto){
         boardService.change(id, boardChangeRequestDto);
     }
 
-
+    /**
+     * 게시물 조회
+     * @param id
+     */
+    @GetMapping("/board/{id}")
+    public Response getBoard(@PathVariable Long id){
+        return Response.success(boardService.getBoard(id));
+    }
 }
