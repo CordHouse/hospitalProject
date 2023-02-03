39cb21b57560222aee1b765a8763216b5ad49ca8
CordHouse <qkqktl5310@gmail.com>
2023-02-03 14:22:02 +0900

    feat : (UsernameValid) - controller or service에서 토큰 인증을 진행하는데, username 같은 경우 자주쓰여 함수로 호출, 추후 어노테이션으로 수정해볼 예정

---
+++ src/main/java/com/example/hospitalproject/Authentication/UsernameValid.java
@@ -0,0 +1,22 @@
+package com.example.hospitalproject.Authentication;
+
+import com.example.hospitalproject.Exception.UserException.NotFoundUserException;
+import com.example.hospitalproject.Repository.User.UserRepository;
+import lombok.Data;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class UsernameValid {
+    private final UserRepository userRepository;
+
+    public Authentication doAuthenticationUsernameCheck() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.findByUsername(authentication.getName()).orElseThrow(() -> {
+            throw new NotFoundUserException("해당 유저가 존재하지 않습니다.");
+        });
+        return authentication;
+    }
+}
