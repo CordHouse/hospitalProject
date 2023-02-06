4f0c6b20b673252205cbcc70e3ef5dfadf48b0c4
JuneBori <jb990628@gmail.com>
2023-02-06 11:03:09 +0900

    feat : (BoardCreateRequestDto) - 게시물 생성 Dto -> starPoint(별점) 기능 구현

--- src/main/java/com/example/hospitalproject/Dto/Board/BoardCreateRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/Board/BoardCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.hospitalproject.Dto.Board;
 
+import com.example.hospitalproject.Exception.Board.NotInputStarPointException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,17 @@ public class BoardCreateRequestDto {
 
     @NotBlank(message = "내용을 입력해주세요.")
     private String content;
+
+    @NotBlank(message = "별점을 입력해주세요.")
+    private String starPoint;
+
+    public Double todo(Double d){
+        d = Double.parseDouble(this.starPoint);
+
+        if(d < 1.0 || d > 5.0) {
+            throw new NotInputStarPointException();
+        }
+
+        return(Double.parseDouble(this.starPoint));
+    }
 }
