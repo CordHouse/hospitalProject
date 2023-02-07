94cad08506b64a25ce28800755594422b0981e3d
JuneBori <jb990628@gmail.com>
2023-02-08 07:27:43 +0900

    refactor : (BoardChangeRequestDto) - starPoint(별점) String -> Double - todo 함수 제거

--- src/main/java/com/example/hospitalproject/Dto/Board/BoardChangeRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/Board/BoardChangeRequestDto.java
@@ -1,6 +1,5 @@
 package com.example.hospitalproject.Dto.Board;
 
-import com.example.hospitalproject.Exception.Board.NotInputStarPointException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,16 +16,5 @@ public class BoardChangeRequestDto {
     @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
-    @NotBlank(message = "별점을 입력해주세요.")
-    private String starPoint;
-
-    public Double todo(Double d){
-        d = Double.parseDouble(this.starPoint);
-
-        if(d < 1.0 || d > 5.0) {
-            throw new NotInputStarPointException();
-        }
-
-        return(Double.parseDouble(this.starPoint));
-    }
+    private Double starPoint;
 }
