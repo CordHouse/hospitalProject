e0aff7f688b2db0baf64239c2c3556c1c0bc9fc2
CordHouse <qkqktl5310@gmail.com>
2023-02-07 23:11:06 +0900

    feat : (ControllerCreate) - createToken 생성

--- src/test/java/com/example/hospitalproject/Controller/Create/ControllerCreate.java
+++ src/test/java/com/example/hospitalproject/Controller/Create/ControllerCreate.java
@@ -3,10 +3,21 @@ package com.example.hospitalproject.Controller.Create;
 import com.example.hospitalproject.Entity.User.RoleUserGrade;
 import com.example.hospitalproject.Entity.User.User;
 import lombok.Builder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collections;
 
 @Builder
 public class ControllerCreate {
     public static User createUser(){
-        return new User("이지우", "jiwoo", "123456a!!", "1999-01-01", "010-1234-1234", "qqq@naver.com", "경기도 수원시", RoleUserGrade.ROLE_COMMON_MEMBER);
+        return new User("Test", "Tester", "123456a!!", "1999-01-01", "010-1234-1234", "qqq@naver.com", "경기도 수원시", RoleUserGrade.ROLE_COMMON_MEMBER);
+    }
+
+    public static Authentication createToken() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(createUser().getId(), "", Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return authentication;
     }
 }
