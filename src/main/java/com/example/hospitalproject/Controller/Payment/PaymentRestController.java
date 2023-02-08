d6b57f9d8fa889c00c883267d2f6b80ea6f0d757
CordHouse <qkqktl5310@gmail.com>
2023-02-08 22:12:45 +0900

    feat : (PaymentRestController) - userTokenValidCheck -> 토큰 유효성 검사

--- src/main/java/com/example/hospitalproject/Controller/Payment/PaymentRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/Payment/PaymentRestController.java
@@ -1,9 +1,14 @@
 package com.example.hospitalproject.Controller.Payment;
 
 import com.example.hospitalproject.Dto.Payment.Card.PayChargeRequestDto;
+import com.example.hospitalproject.Entity.User.User;
+import com.example.hospitalproject.Exception.UserException.NotFoundUserException;
+import com.example.hospitalproject.Repository.User.UserRepository;
 import com.example.hospitalproject.Service.Payment.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +18,7 @@ import javax.validation.Valid;
 @RequestMapping("/payment")
 public class PaymentRestController {
     private final PaymentService paymentService;
+    private final UserRepository userRepository;
 
     /**
      * 결제 진행
@@ -20,7 +26,8 @@ public class PaymentRestController {
     @PostMapping("/common/charge")
     @ResponseStatus(HttpStatus.OK)
     public void payCharge(@RequestBody @Valid PayChargeRequestDto payChargeRequestDto){
-        paymentService.payCharge(payChargeRequestDto);
+        User user = userTokenValidCheck();
+        paymentService.payCharge(payChargeRequestDto, user);
     }
 
     /**
@@ -29,7 +36,8 @@ public class PaymentRestController {
     @PostMapping("/common/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void payCancel(@PathVariable long id){
-        paymentService.payCancel(id);
+        User user = userTokenValidCheck();
+        paymentService.payCancel(id, user);
     }
 
     /**
@@ -38,6 +46,14 @@ public class PaymentRestController {
     @PostMapping("/manager/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void payToDoApproval(@PathVariable long id){
-        paymentService.payToDoApproval(id);
+        User user = userTokenValidCheck();
+        paymentService.payToDoApproval(id, user);
+    }
+
+    private User userTokenValidCheck() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByUsername(authentication.getName()).orElseThrow(() -> {
+            throw new NotFoundUserException("해당 유저가 존재하지 않습니다.");
+        });
     }
 }
