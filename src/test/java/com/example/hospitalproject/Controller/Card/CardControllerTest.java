7024d9f59390e5f721a9aaab37fb0540ac30c8b7
CordHouse <qkqktl5310@gmail.com>
2023-02-08 21:52:18 +0900

    feat : (Test/CardControllerTest) - testUserValidSet 추가 - UserRepository 추가

--- src/test/java/com/example/hospitalproject/Controller/Card/CardControllerTest.java
+++ src/test/java/com/example/hospitalproject/Controller/Card/CardControllerTest.java
@@ -2,6 +2,8 @@ package com.example.hospitalproject.Controller.Card;
 
 import com.example.hospitalproject.Controller.Payment.Card.CardRestController;
 import com.example.hospitalproject.Dto.Payment.Card.CardInfoRequestDto;
+import com.example.hospitalproject.Entity.User.User;
+import com.example.hospitalproject.Repository.User.UserRepository;
 import com.example.hospitalproject.Service.Payment.Card.CardService;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,11 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Optional;
+
+import static com.example.hospitalproject.Controller.Create.ControllerCreate.createToken;
+import static com.example.hospitalproject.Controller.Create.ControllerCreate.createUser;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,6 +33,9 @@ public class CardControllerTest {
     private CardRestController cardRestController;
     @Mock // Mock 생성
     private CardService cardService;
+
+    @Mock
+    private UserRepository userRepository;
 
     // Controller 테스트시 HTTP 호출이 필요한데, 일반적으론 불가능 하기 때문에 아래와 같은 변수와 함수로 생성해야 한다.
     private MockMvc mockMvc;
@@ -43,17 +53,18 @@ public class CardControllerTest {
     void cardRegistrationTest() throws Exception {
         // given
         CardInfoRequestDto cardInfoRequestDto = cardCreate();
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.post("/payment/card/registration")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new Gson().toJson(cardInfoRequestDto))
-        );
+        ).andExpect(status().isOk());
 
         // then
-        resultActions.andExpect(status().isOk()).andReturn();
-        verify(cardService).cardRegistration(cardInfoRequestDto);
+        verify(cardService).cardRegistration(cardInfoRequestDto, user);
     }
 
     /**
@@ -61,17 +72,19 @@ public class CardControllerTest {
      */
     @DisplayName("등록한 카드 중 사용할 카드 선택하기")
     @Test
-    void cardChoice() throws Exception {
+    void cardChoiceTest() throws Exception {
         // given
         long id = 1L;
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.post("/payment/card/choice/"+id)
-        );
+        ).andExpect(status().isOk());
 
         // then
-        resultActions.andExpect(status().isOk()).andReturn();
+        verify(cardService).cardChoice(id, user);
     }
 
     /**
@@ -79,16 +92,18 @@ public class CardControllerTest {
      */
     @DisplayName("등록한 카드 리스트 조회")
     @Test
-    void getMyCardList() throws Exception {
+    void getMyCardListTest() throws Exception {
         // given
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.get("/payment/card/my/list")
-        );
+        ).andExpect(status().isOk());
 
         // then
-        resultActions.andExpect(status().isOk());
+        verify(cardService).getMyCardList(user);
     }
 
     /**
@@ -96,16 +111,18 @@ public class CardControllerTest {
      */
     @DisplayName("현재 선택으로 되어 있는 카드 조회")
     @Test
-    void getMyCard() throws Exception {
+    void getMyCardTest() throws Exception {
         // given
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.get("/payment/card/my")
-        );
+        ).andExpect(status().isOk());
 
         // then
-        resultActions.andExpect(status().isOk());
+        verify(cardService).getMyCard(user);
     }
 
     /**
@@ -113,17 +130,19 @@ public class CardControllerTest {
      */
     @DisplayName("선택한 카드 변경")
     @Test
-    void changeMyChoiceCard() throws Exception {
+    void changeMyChoiceCardTest() throws Exception {
         // given
         long id = 1L;
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.put("/payment/card/my/"+id)
-        );
+        ).andExpect(status().isOk());
 
         // then
-        resultActions.andExpect(status().isOk());
+        verify(cardService).changeMyChoiceCard(id, user);
     }
 
     /**
@@ -131,17 +150,19 @@ public class CardControllerTest {
      */
     @DisplayName("하나의 카드가 등록되어 있고 미선택으로 바꾸고 싶은 경우")
     @Test
-    void cardChoiceChange() throws Exception {
+    void cardChoiceChangeTest() throws Exception {
         // given
         long id = 1L;
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.put("/payment/card/status/"+id)
-        );
+        ).andExpect(status().isOk());
 
         // then
-        resultActions.andExpect(status().isOk());
+        verify(cardService).cardChoiceChange(id, user);
     }
 
     /**
@@ -149,16 +170,18 @@ public class CardControllerTest {
      */
     @DisplayName("등록한 카드 삭제")
     @Test
-    void deleteRegistrationCard() throws Exception {
+    void deleteRegistrationCardTest() throws Exception {
         // given
         long id = 1L;
+        User user = createUser();
+        testUserValidSet(user);
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.delete("/payment/card/"+id)
-        );
+        ).andExpect(status().isOk());
         // then
-        resultActions.andExpect(status().isOk());
+        verify(cardService).deleteRegistrationCard(id, user);
     }
 
     private CardInfoRequestDto cardCreate(){
@@ -170,5 +193,10 @@ public class CardControllerTest {
                 .password("12")
                 .selectCard("선택")
                 .build();
+    }
+
+    void testUserValidSet(User user) {
+        Authentication authentication = createToken();
+        given(userRepository.findByUsername(authentication.getName())).willReturn(Optional.of(user));
     }
 }
