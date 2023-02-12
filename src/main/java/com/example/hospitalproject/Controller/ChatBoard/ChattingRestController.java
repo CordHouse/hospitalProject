83e2d3d73442ce71c589d51a8c17d933f139b387
CordHouse <qkqktl5310@gmail.com>
2023-02-12 17:01:15 +0900

    feat : (ChattingRestController) - Swagger 설명 추가

--- src/main/java/com/example/hospitalproject/Controller/ChatBoard/ChattingRestController.java
+++ src/main/java/com/example/hospitalproject/Controller/ChatBoard/ChattingRestController.java
@@ -6,6 +6,8 @@ import com.example.hospitalproject.Exception.UserException.NotFoundUserException
 import com.example.hospitalproject.Repository.User.UserRepository;
 import com.example.hospitalproject.Response.Response;
 import com.example.hospitalproject.Service.ChatBoard.ChattingService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -24,6 +26,8 @@ public class ChattingRestController {
     /**
      * 채팅방(id)에서 채팅하기
      */
+    @ApiOperation(value = "채팅시작", notes = "상대방과 채팅하기 위해 사용")
+    @ApiImplicitParam(name = "id", value = "채팅방 번호", example = "0")
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     private void chatRunStatus(@PathVariable Long id, @RequestBody @Valid ChattingCommentRequestDto chattingCommentRequestDto){
@@ -34,6 +38,8 @@ public class ChattingRestController {
     /**
      * 채팅방 안의 채팅 조회
      */
+    @ApiOperation(value = "채팅방 채팅조회", notes = "채팅방 내부 채팅 조회시 사용")
+    @ApiImplicitParam(name = "id", value = "채팅방 번호", example = "0")
     @GetMapping("/{id}/comment")
     @ResponseStatus(HttpStatus.OK)
     private Response getMyChattingList(@PathVariable long id){
@@ -44,6 +50,8 @@ public class ChattingRestController {
     /**
      * 채팅 삭제
      */
+    @ApiOperation(value = "채팅삭제", notes = "채팅내용 삭제하기 위해 사용")
+    @ApiImplicitParam(name = "id", value = "채팅방 번호", example = "0")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     private void chatDelete(@PathVariable Long id){
