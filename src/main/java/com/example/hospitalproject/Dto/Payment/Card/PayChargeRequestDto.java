a44fd75688d526219bfb219e90765c3f5eceeca8
CordHouse <qkqktl5310@gmail.com>
2023-02-12 17:01:47 +0900

    feat : (PayChargeRequestDto) - Swagger 설명 추가

--- src/main/java/com/example/hospitalproject/Dto/Payment/Card/PayChargeRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/Payment/Card/PayChargeRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.hospitalproject.Dto.Payment.Card;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,11 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @Builder
 public class PayChargeRequestDto {
+    @ApiModelProperty(value = "결제 금액", example = "10000")
     @NotNull(message = "결제 금액을 입력해주세요.")
     private int pay;
 
+    @ApiModelProperty(value = "결제 방법", example = "신용카드/계좌이체/..")
     @NotBlank(message = "결제 하실 방법을 선택해주세요.")
     private String code;
 }
