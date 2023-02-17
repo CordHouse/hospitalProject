add564d7810eea115b50a546e282fe9787301948
CordHouse <qkqktl5310@gmail.com>
2023-02-17 15:03:47 +0900

    refactor : (EmailRepository) - 토큰 만료시간 추가하여 수정 5분

--- src/main/java/com/example/hospitalproject/Repository/Email/EmailRepository.java
+++ src/main/java/com/example/hospitalproject/Repository/Email/EmailRepository.java
@@ -4,11 +4,12 @@ import com.example.hospitalproject.Entity.Email.EmailToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface EmailRepository extends JpaRepository<EmailToken, Long> {
-    Optional<EmailToken> findByIdAndExpired(String emailTokenId, String expired);
+    Optional<EmailToken> findByIdAndExpirationDateAfterAndExpired(String emailTokenId, LocalDateTime now, String expired);
 
     Optional<EmailToken> findByUsername(String username);
 }
