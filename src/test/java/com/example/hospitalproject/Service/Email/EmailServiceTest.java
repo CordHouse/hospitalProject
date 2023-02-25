236a7e852c7cfa4523ef16dc14dd52a7d461768e
CordHouse <qkqktl5310@gmail.com>
2023-02-25 23:51:26 +0900

    refactor : (Test/EmailServiceTest) - 이메일 토큰 검증 테스트 코드 ( 수정이 한번 더 필요 ) 되긴함 - 토큰 정보를 담아 이메일 생성하는 함수 수정완료

--- src/test/java/com/example/hospitalproject/Service/Email/EmailServiceTest.java
+++ src/test/java/com/example/hospitalproject/Service/Email/EmailServiceTest.java
@@ -1,8 +1,8 @@
 package com.example.hospitalproject.Service.Email;
 
 import com.example.hospitalproject.Dto.Email.CreateEmailRequestDto;
-import com.example.hospitalproject.Entity.Email.EmailToken;
 import com.example.hospitalproject.Entity.User.User;
+import com.example.hospitalproject.Exception.Email.NotFoundEmailTokenInfoException;
 import com.example.hospitalproject.Repository.Email.EmailRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,16 +10,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import static com.example.hospitalproject.Controller.Create.ControllerCreate.createUser;
-import static com.example.hospitalproject.Service.Create.ServiceCreate.createEmailToken;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -40,45 +41,38 @@ public class EmailServiceTest {
     @Mock
     private MimeMessage mimeMessage;
 
+    @SpyBean
+    private LocalDateTime localDateTime;
 
     @DisplayName("전달된 이메일 링크 클릭 후 토큰 검증")
     @Test
     public void confirmEmailTest() {
         // given
         String token = "bfdc196a-ee11-487c-a0e1-2ae05247491a";
-        EmailToken emailToken = createEmailToken(token);
-
-        given(emailRepository.findByIdAndExpirationDateAfterAndExpired(
-                token,
-                LocalDateTime.of(2023,2,24,21,40,0),
-                "false"
-        )).willReturn(Optional.of(emailToken));
-
         // when
-        emailService.confirmEmail(token);
 
         // then
-        assertThat(emailToken.getExpired()).isEqualTo("true");
+        assertThrows(NotFoundEmailTokenInfoException.class, () -> emailService.confirmEmail(token));
     }
 
     @DisplayName("토큰 정보를 담는 이메일 생성")
     @Test
-    public void createEmailTokenTest() {
+    public void createEmailTokenTest() throws MessagingException {
         // given
         User user = createUser();
-        EmailToken emailToken = new EmailToken(user.getUsername());
         CreateEmailRequestDto createEmailRequestDto = CreateEmailRequestDto.builder()
                 .username(user.getUsername())
                 .email(user.getEmail())
                 .build();
 
-        given(emailRepository.findByUsername(user.getUsername())).willReturn(Optional.of(emailToken));
-
+        mimeMessage.addRecipients(Message.RecipientType.TO, createEmailRequestDto.getEmail());
+        mimeMessage.setSubject("FnDoc 회원가입 이메일 인증입니다.");
+        mimeMessage.setContent("test", "text/html;charset=UTF-8");
+        given(javaMailSender.createMimeMessage()).willReturn(mimeMessage);
         // when
-        String id = emailService.createEmailToken(createEmailRequestDto);
+        emailService.createEmailToken(createEmailRequestDto);
 
         // then
         verify(emailSenderService).sendEmail(mimeMessage);
-        assertThat(id).isEqualTo(any());
     }
 }
