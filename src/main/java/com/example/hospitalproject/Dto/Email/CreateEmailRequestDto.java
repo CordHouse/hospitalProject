4dc119436b424c02834c63eaaf2175a0f9ef058e
CordHouse <qkqktl5310@gmail.com>
2023-02-27 18:00:27 +0900

    feat : (CreateEmailRequestDto) - 이메일 요청 스웨거 작성

--- src/main/java/com/example/hospitalproject/Dto/Email/CreateEmailRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/Email/CreateEmailRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.hospitalproject.Dto.Email;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +13,10 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 @Builder
 public class CreateEmailRequestDto {
+    @ApiModelProperty(value = "아이디 입력", example = "tester")
     @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
+    @ApiModelProperty(value = "이메일 입력", example = "example@naver.com")
     @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
 }
