e3d389f993e1212f9938b1cb9444597ca17f77d4
minsub <chrkb1569@naver.com>
2023-03-02 01:07:47 +0900

    feat : Add Swagger Options to DTO Files

--- src/main/java/com/example/hospitalproject/Dto/Token/TokenReIssueDto.java
+++ src/main/java/com/example/hospitalproject/Dto/Token/TokenReIssueDto.java
@@ -1,5 +1,6 @@
 package com.example.hospitalproject.Dto.Token;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,11 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenReIssueDto {
+    @ApiModelProperty(value = "AccessToken의 유효 기간을 연장하기 위한 originToken", example = "originToken")
     @NotBlank(message = "originToken 값을 입력해주세요")
     private String originToken;
 
+    @ApiModelProperty(value = "AccessToken의 유효 기간을 연장하기 위한 refreshToken", example = "refreshToken")
     @NotBlank(message = "refreshToken 값을 입력해주세요")
     private String refreshToken;
 }
--- src/main/java/com/example/hospitalproject/Dto/User/UserGradeSearchRequestDto.java
+++ src/main/java/com/example/hospitalproject/Dto/User/UserGradeSearchRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.hospitalproject.Dto.User;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ import javax.validation.constraints.Pattern;
 public class UserGradeSearchRequestDto {
     @NotBlank(message = "닉네임을 작성해주세요.")
     @Pattern(regexp = "^[0-9a-zA-Z]{2,10}", message = "입력하신 아이디를 다시 한 번 확인해주세요")
+    @ApiModelProperty(value = "권한을 확인하기 위한 사용자의 아이디 입력", example = "test1234")
     private String username;
 }
